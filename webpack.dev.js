05df1fb748d305394944de8b4101c8b8414225b3
Paweł Owczarek <pawel@ow.cx>
2019-04-12 14:48:21 +0200

    Add webpack configuration files

---
+++ webpack.dev.js
@@ -0,0 +1,18 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const webpack = require('webpack');
+
+module.exports = merge(common, {
+  mode: 'development',
+  optimization: {
+    usedExports: true
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  }
+});
