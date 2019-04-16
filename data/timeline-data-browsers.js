fdc571192363327ddec6836f58dfd1050e5b4249
Paweł Owczarek <pawel@ow.cx>
2019-04-16 22:56:10 +0200

    Add details for Opera browser

--- data/timeline-data-browsers.js
+++ data/timeline-data-browsers.js
@@ -5,7 +5,7 @@ const
     FIREFOX_WIKI = 'https://en.wikipedia.org/wiki/Firefox',
     CHROMIUM = 'Chromium',
     CHROMIUM_WIKI = 'https://en.wikipedia.org/wiki/Chromium_(web_browser)',
-    CHROME = 'Google Chrome',
+    CHROME = 'Chrome',
     CHROME_WIKI = 'https://en.wikipedia.org/wiki/Google_Chrome',
     IE = 'Internet Explorer',
     IE_WIKI = 'https://en.wikipedia.org/wiki/Internet_Explorer',
@@ -37,6 +37,10 @@ const
     EDGEHTML_WIKI = 'https://en.wikipedia.org/wiki/EdgeHTML',
     NITRO = 'Nitro',
     NITRO_WIKI = 'https://en.wikipedia.org/wiki/WebKit#JavaScriptCore',
+    PRESTO = 'Presto',
+    PRESTO_WIKI = 'https://en.wikipedia.org/wiki/Presto_(browser_engine)',
+    CARAKAN = 'Carakan',
+    CARAKAN_WIKI = 'https://en.wikipedia.org/wiki/Presto_(browser_engine)#ECMAScript_engines',
     BROWSERS_CONFIG = {
         groupOrder: 'order',
         min: '1995-01-01',
@@ -62,22 +66,22 @@ const
             title: `${CHROMIUM}`
         },
         {
-            id: 3, order: 3,
+            id: 3, order: 4,
             content: `<a href=${FIREFOX_WIKI}>${FIREFOX}</a>`,
             title: `${FIREFOX}`
         },
         {
-            id: 4, order: 4,
+            id: 4, order: 5,
             content: `<a href=${IE_WIKI}>${IE}</a>`,
             title: `${IE}`
         },
         {
-            id: 5, order: 5,
+            id: 5, order: 3,
             content: `<a href=${EDGE_WIKI}>${EDGE}</a>`,
             title: `${EDGE}`
         },
         {
-            id: 6, order: 6,
+            id: 6, order: 8,
             content: `<a href=${SAFARI_WIKI}>${SAFARI}</a>`,
             title: `${SAFARI}`
         },
@@ -87,14 +91,14 @@ const
             title: `${OPERA}`
         },
         {
-            id: 8, order: 8,
+            id: 8, order: 6,
             content: `<a href=${LYNX_WIKI}>${LYNX}</a>`,
             title: `${LYNX}`
         }
     ],
     BROWSERS_CHROME = [
         {
-            id: 10, group: 1,
+            id: 100, group: 1,
             content: `<a href=${BLINK_WIKI}>${BLINK}</a>`,
             title: `${BLINK}`,
             start: '2013-04-03',
@@ -102,7 +106,7 @@ const
             className:'browsers-layout-engine'
         },
         {
-            id: 11, group: 1,
+            id: 101, group: 1,
             content: `<a href=${WEBKIT_WIKI}>${WEBKIT}</a>`,
             title: `${WEBKIT}`,
             start: '2008-09-02',
@@ -110,7 +114,7 @@ const
             className:'browsers-layout-engine'
         },
         {
-            id: 12, group: 1,
+            id: 102, group: 1,
             content: `<a href=${V8_WIKI}>${V8}</a>`,
             title: `${V8}`,
             start: '2008-09-02',
@@ -120,7 +124,7 @@ const
     ],
     BROWSERS_FIREFOX = [
         {
-            id: 30, group: 3,
+            id: 300, group: 3,
             content: `<a href=${GECKO_WIKI}>${GECKO}</a>`,
             title:'Gecko',
             start: '1997-01-01',
@@ -128,7 +132,7 @@ const
             className:'browsers-layout-engine'
         },
         {
-            id: 31, group: 3,
+            id: 301, group: 3,
             content:
               `<a href=${SPIDER_MONKEY_WIKI}>${SPIDER_MONKEY}</a>`,
             title:`${SPIDER_MONKEY}`,
@@ -139,7 +143,7 @@ const
     ],
     BROWSERS_CHROMIUM = [
         {
-            id: 20, group: 2,
+            id: 200, group: 2,
             content: `<a href=${BLINK_WIKI}>${BLINK}</a>`,
             title: `${BLINK}`,
             start: '2013-04-03',
@@ -147,7 +151,7 @@ const
             className:'browsers-layout-engine'
         },
         {
-            id: 21, group: 2,
+            id: 201, group: 2,
             content: `<a href=${WEBKIT_WIKI}>${WEBKIT}</a>`,
             title: `${WEBKIT}`,
             start: '2008-09-02',
@@ -155,7 +159,7 @@ const
             className:'browsers-layout-engine'
         },
         {
-            id: 22, group: 2,
+            id: 202, group: 2,
             content: `<a href=${V8_WIKI}>${V8}</a>`,
             title: `${V8}`,
             start: '2008-09-02',
@@ -165,7 +169,7 @@ const
     ],
     BROWSERS_IE = [
         {
-            id: 40, group: 4,
+            id: 400, group: 4,
             content: `<a href=${TRIDENT_WIKI}>${TRIDENT}</a>`,
             title: `${TRIDENT}`,
             start: '1995-08-16',
@@ -173,7 +177,7 @@ const
             className:'browsers-layout-engine'
         },
         {
-            id: 41, group: 4,
+            id: 401, group: 4,
             content: `<a href=${JSCRIPT_WIKI}>${JSCRIPT}</a>`,
             title: `${JSCRIPT}`,
             start: '1995-08-16',
@@ -181,7 +185,7 @@ const
             className:'browsers-js-engine'
         },
         {
-            id: 42, group: 4,
+            id: 402, group: 4,
             content: `<a href=${CHAKRA_WIKI}>${CHAKRA}</a>`,
             title: `${CHAKRA}`,
             start: '1995-08-16',
@@ -191,7 +195,7 @@ const
     ],
     BROWSERS_EDGE = [
         {
-            id: 50, group: 5,
+            id: 500, group: 5,
             content: `<a href=${EDGEHTML_WIKI}>${EDGEHTML}</a>`,
             title: `${EDGEHTML}`,
             start: '2015-07-29',
@@ -199,7 +203,7 @@ const
             className:'browsers-layout-engine'
         },
         {
-            id: 51, group: 5,
+            id: 501, group: 5,
             content: `<a href=${BLINK_WIKI}>${BLINK}</a>`,
             title: `${BLINK}`,
             start: '2019-01-01',
@@ -207,7 +211,7 @@ const
             className:'browsers-layout-engine'
         },
         {
-            id: 53, group: 5,
+            id: 503, group: 5,
             content: `<a href=${CHAKRA_WIKI}>${CHAKRA}</a>`,
             title: `${CHAKRA}`,
             start: '2015-07-29',
@@ -215,7 +219,7 @@ const
             className:'browsers-js-engine'
         },
         {
-            id: 54, group: 5,
+            id: 504, group: 5,
             content: `<a href=${V8_WIKI}>${V8}</a>`,
             title: `${V8}`,
             start: '2019-01-01',
@@ -225,7 +229,7 @@ const
     ],
     BROWSERS_SAFARI = [
         {
-            id: 60, group: 6,
+            id: 600, group: 6,
             content: `<a href=${WEBKIT_WIKI}>${WEBKIT}</a>`,
             title: `${WEBKIT}`,
             start: '2003-01-07',
@@ -233,29 +237,71 @@ const
             className:'browsers-layout-engine'
         },
         {
-            id: 61, group: 6,
+            id: 601, group: 6,
             content: `<a href=${NITRO_WIKI}>${NITRO}</a>`,
             title: `${NITRO}`,
             start: '2003-01-07',
             end:DATE_NOW,
             className:'browsers-js-engine'
         }
+    ],
+    BROWSERS_OPERA = [
+        {
+            id: 700, group: 7,
+            content: `<a href=${PRESTO_WIKI}>${PRESTO}</a>`,
+            title: `${PRESTO}`,
+            start: '1995-04-10',
+            end: '2013-02-12',
+            className:'browsers-layout-engine'
+        },
+        {
+            id: 701, group: 7,
+            content: `<a href=${WEBKIT_WIKI}>${WEBKIT}</a>`,
+            title: `${WEBKIT}`,
+            start: '2013-02-13',
+            end: '2013-04-03',
+            className:'browsers-layout-engine'
+        },
+        {
+            id: 702, group: 7,
+            content: `<a href=${BLINK_WIKI}>${BLINK}</a>`,
+            title: `${BLINK}`,
+            start: '2013-04-04',
+            end: DATE_NOW,
+            className:'browsers-layout-engine'
+        },
+        {
+            id: 703, group: 7,
+            content: `<a href=${CARAKAN_WIKI}>${CARAKAN}</a>`,
+            title: `${CARAKAN}`,
+            start: '1995-04-10',
+            end: '2013-05-27',
+            className:'browsers-js-engine'
+        },
+        {
+            id: 704, group: 7,
+            content: `<a href=${V8_WIKI}>${V8}</a>`,
+            title: `${V8}`,
+            start: '2013-05-28',
+            end: DATE_NOW,
+            className:'browsers-js-engine'
+        }
     ]
     // BROWSERS_ = [
     //     {
-    //         id: 3, group: 1,
+    //         id: , group: ,
     //         content: `<a href=${}>${}</a>`,
     //         title: `${}`,
-    //         start: '1970-07-01',
-    //         end:'1970-10-31',
+    //         start: '',
+    //         end:'',
     //         className:'browsers-layout-engine'
     //     },
     //     {
-    //         id: 4, group: 1,
+    //         id: , group: ,
     //         content: `<a href=${}>${}</a>`,
     //         title: `${}`,
-    //         start: '1970-01-01',
-    //         end:'1970-12-31',
+    //         start: '',
+    //         end:'',
     //         className:'browsers-js-engine'
     //     }
     // ]
@@ -268,6 +314,7 @@ module.exports = {
       BROWSERS_FIREFOX,
       BROWSERS_IE,
       BROWSERS_EDGE,
-      BROWSERS_SAFARI
+      BROWSERS_SAFARI,
+      BROWSERS_OPERA
     )
 };
